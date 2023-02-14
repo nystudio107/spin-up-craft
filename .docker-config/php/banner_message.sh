6c279246aef4cc39534042567554f150104aaf1b
Andrew Welch <andrew@nystudio107.com>
2023-02-14 11:18:55 -0500

    refactor: Refactor the banner to a separate `banner_message.sh`

---
+++ .docker-config/php/banner_message.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Banner Message shell script
+#
+# Display a banner message to let the user know the environment is ready for use
+#
+# @author    nystudio107
+# @copyright Copyright (c) 2023 nystudio107
+# @link      https://nystudio107.com/
+# @license   MIT
+
+# Rewrite the PRIMARY_SITE_URL env var if we're running in Codespaces
+if [[ ! ${CODESPACES:-"unset"} == "unset" ]]; then
+    export PRIMARY_SITE_URL="https://${CODESPACE_NAME}-${DEV_SERVER_PORT}.preview.app.github.dev/"
+fi
+# Banner message
+sleep 1
+echo "### Your Craft site is ready!"
+echo "Frontend URL: ${PRIMARY_SITE_URL}"
+echo "CP URL: ${PRIMARY_SITE_URL}admin"
+echo "CP User: ${CRAFT_CP_USER}"
+echo "CP Password: ${CRAFT_CP_PASSWORD}"
